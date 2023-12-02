db0267cb24e4eafd9c1b80059d429715e4df2e21
yegor <astery227@gmail.com>
2023-12-02 09:58:30 +0300

    fix: stop shrinking highlighted edge dates

--- src/Calendar/Highlighted.tsx
+++ src/Calendar/Highlighted.tsx
@@ -2,43 +2,73 @@ import classNames from "classnames";
 import { endOfMonth, isEqual, isSaturday, isSunday } from "date-fns";
 import { useCalendar } from "./CalendarProvider";
 
+const FIRST_DATE = "rounded-l-full left-2";
+const SECOND_DATE = "rounded-r-full right-2";
+const LEFT_EDGE = "rounded-l-full left-[0.3rem]";
+const RIGHT_EDGE = "rounded-r-full right-[0.3rem]";
+
 export default function Highlighted({ day }: { day: Date }) {
   const { firstDate, secondDate } = useCalendar();
+  let leftSideChanged = false;
+  let rightSideChanged = false;
 
   function getHighlighted() {
-    const highlighted = "bg-blue-200/50 border-blue-400";
+    let highlighted = "bg-blue-200/50 border-blue-400";
     if (firstDate && secondDate) {
       if (day >= firstDate && day <= secondDate) {
-        return getHighlightedStyling(highlighted);
+        return classNames(highlighted, getStylingsForDifferentDates());
       }
     }
 
     return "";
   }
 
-  function getHighlightedStyling(highlighted: string) {
-    if (
-      (firstDate && isEqual(day, firstDate)) ||
-      isSunday(day) ||
-      day.getDate() === 1
-    ) {
-      highlighted = classNames(highlighted, "rounded-l-full left-2");
-    }
-    if (
-      (secondDate && isEqual(day, secondDate)) ||
-      isSaturday(day) ||
-      day.getDate() === endOfMonth(day).getDate()
-    ) {
-      highlighted = classNames(highlighted, "rounded-r-full right-2");
-    }
-
+  function getStylingsForDifferentDates() {
+    let highlighted = "";
+    highlighted = applyStylingForSelectedDates(highlighted);
+    highlighted = applyStylingForEdgeDates(highlighted);
+    highlighted = applySideOffsetsIfUnchanged(highlighted);
     return highlighted;
+  }
+
+  function applyStylingForSelectedDates(style: string) {
+    if (firstDate && isEqual(day, firstDate)) {
+      style = classNames(style, FIRST_DATE);
+      leftSideChanged = true;
+    }
+    if (secondDate && isEqual(day, secondDate)) {
+      style = classNames(style, SECOND_DATE);
+      rightSideChanged = true;
+    }
+    return style;
+  }
+
+  function applyStylingForEdgeDates(style: string) {
+    if (isSunday(day) || day.getDate() === 1) {
+      style = classNames(style, LEFT_EDGE);
+      leftSideChanged = true;
+    }
+    if (isSaturday(day) || day.getDate() === endOfMonth(day).getDate()) {
+      style = classNames(style, RIGHT_EDGE);
+      rightSideChanged = true;
+    }
+    return style;
+  }
+
+  function applySideOffsetsIfUnchanged(style: string) {
+    if (!leftSideChanged) {
+      style = classNames(style, "left-0");
+    }
+    if (!rightSideChanged) {
+      style = classNames(style, "right-0");
+    }
+    return style;
   }
 
   return (
     <div
-      className={`absolute top-[3px] left-0 bottom-[3px] right-0
-        flex items-center justify-center ${getHighlighted()}`}
+      className={`absolute top-[3px] bottom-[3px] flex items-center 
+      justify-center ${getHighlighted()}`}
     ></div>
   );
 }
