60fcf65f6121a5efb9e76aebf4e1e070b3844b0e
yegor <astery227@gmail.com>
2023-12-08 16:21:21 +0300

    feat(Calendar): display the month of the active input

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,8 +1,9 @@
-import { addMonths, subMonths } from "date-fns";
+import { addMonths, isSameMonth, subMonths } from "date-fns";
 import { useEffect, useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { useDateTimeRange } from "./DateTimeRangeProvider";
+import { ActiveInput } from "../types";
 import Cells from "./Cells";
+import { useDateTimeRange } from "./DateTimeRangeProvider";
 import Days from "./Days";
 import Header from "./Header";
 import "./styles/Calendar.css";
@@ -10,31 +11,65 @@ import "./styles/Calendar.css";
 const duration = 250;
 
 function Calendar() {
-  const { currentMonth, onCurrentMonthChange, isDragging, onIsDraggingChange } =
-    useDateTimeRange();
+  const {
+    currentMonth,
+    onCurrentMonthChange,
+    isDragging,
+    onIsDraggingChange,
+    activeInput,
+    firstDate,
+    secondDate,
+  } = useDateTimeRange();
   const [isNext, setIsNext] = useState(true);
   const [cellsComponents, setCellsComponents] = useState<
     { id: string; element: JSX.Element; isNext: boolean }[]
   >([]);
-  const [shouldApplyTransition, setShouldApplyTransition] = useState(false);
+  const [isCellsFirstRender, setIsCellsFirstRender] = useState(true);
 
   useEffect(() => {
-    if (!shouldApplyTransition && cellsComponents.length === 1) {
-      setShouldApplyTransition(true);
+    let newCurrentMonth = currentMonth;
+    if (activeInput === ActiveInput.First && firstDate) {
+      newCurrentMonth = firstDate;
+    } else if (activeInput === ActiveInput.Second && secondDate) {
+      newCurrentMonth = secondDate;
+    }
+
+    setIsNext(newCurrentMonth > currentMonth);
+    onCurrentMonthChange(newCurrentMonth);
+  }, [activeInput]);
+
+  useEffect(() => {
+    if (isCellsFirstRender && cellsComponents.length === 1) {
+      setIsCellsFirstRender(false);
     }
   }, [cellsComponents]);
 
   useEffect(() => {
-    const key = currentMonth.toDateString();
+    if (shouldUpdateCells()) {
+      const key =
+        currentMonth.getMonth().toString() +
+        currentMonth.getFullYear().toString();
 
-    const newComponent = {
-      id: key,
-      element: <Cells currentMonth={currentMonth} key={key} />,
-      isNext: isNext,
-    };
+      const newComponent = {
+        id: key,
+        element: <Cells currentMonth={currentMonth} key={key} />,
+        isNext: isNext,
+      };
 
-    setCellsComponents([newComponent]);
+      setCellsComponents([newComponent]);
+    }
   }, [currentMonth]);
+
+  function shouldUpdateCells() {
+    if (isCellsFirstRender) {
+      if (activeInput === ActiveInput.First && firstDate) {
+        return isSameMonth(firstDate, currentMonth);
+      } else if (activeInput === ActiveInput.Second && secondDate) {
+        return isSameMonth(secondDate, currentMonth);
+      }
+    }
+    return true;
+  }
 
   function onPrevMonthClick() {
     onCurrentMonthChange(subMonths(currentMonth, 1));
@@ -64,9 +99,9 @@ function Calendar() {
           <CSSTransition
             timeout={duration}
             classNames={
-              !shouldApplyTransition
-                ? ""
-                : `${isNext ? "slide-next" : "slide-prev"}`
+              !isCellsFirstRender
+                ? `${isNext ? "slide-next" : "slide-prev"}`
+                : ""
             }
             key={cell.id}
           >
